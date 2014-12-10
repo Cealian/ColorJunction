f27d567d52e91e1d5b217edc51775348b8aa2bfe
Linus rudbeck <rudbeck.linus@gmail.com>
2014-12-10 13:11:39 +0100

    dålig fillfunktion implementerad

--- ColorJunction/MainWindow.xaml.cs
+++ ColorJunction/MainWindow.xaml.cs
@@ -23,6 +23,45 @@ namespace ColorJunction
         public MainWindow()
         {
             InitializeComponent();
+            fillGrid();
+        }
+
+        private void fillGrid() 
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Rectangle rect = new Rectangle();
+
+                rect.Height = 20;
+                rect.Width = 20;
+
+                Brush col;
+                switch (i%4)
+                {
+                    case 0:
+                        col = Brushes.Blue;
+                        break;
+                    case 1:
+                        col = Brushes.Yellow;
+                        break;
+                    case 2:
+                        col = Brushes.Green;
+                        break;
+                    default:
+                        col = Brushes.Red;
+                        break;
+                }
+
+                rect.Fill = col;
+
+                gameCanvas.Children.Add(rect);
+
+                double top = gameCanvas.Height - (20 * ((i % 10) + 1));
+                double left = 0;
+
+                Canvas.SetTop(rect, top);
+                Canvas.SetLeft(rect, left);
+            }
         }
     }
 }
