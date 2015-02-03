3b05f7fa91dd5dd15452336965c0c190065df946
Elias <elias.johanson@gmail.com>
2015-02-03 21:57:15 +0100

    plocka bort onödig fil

--- ColorJunction/Properties/Resources.Designer.cs
+++ ColorJunction/Properties/Resources.Designer.cs
@@ -671,16 +671,6 @@ namespace ColorJunction.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] Highscore {
-            get {
-                object obj = ResourceManager.GetObject("Highscore", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap RecBlå {
